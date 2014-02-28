213e98392945c59b92adb87d319840bd81be9d99
Anup Biswal <anup.craver@gmail.com>
2014-02-28 18:50:41 +0530

    Incorporated some of Russell's feedbacks

--- public/javascripts/erp_app/desktop/applications/stock_finder_app/module.js
+++ public/javascripts/erp_app/desktop/applications/stock_finder_app/module.js
@@ -14,7 +14,6 @@ Ext.define("Compass.ErpApp.Desktop.Applications.StockFinderApp",{
     var desktop = this.app.getDesktop();
     var win = desktop.getWindow('stock_finder_app');
     if(!win){
-      var stockList = createStockList();
       win = desktop.createWindow({
         id: 'stock_finder_app',
         title:'Stock Finder App',
@@ -26,87 +25,89 @@ Ext.define("Compass.ErpApp.Desktop.Applications.StockFinderApp",{
         animCollapse:false,
         constrainHeader:true,
         items:[
-          createStockFinderForm(stockList), stockList
+          {xtype: 'stockfinderapp-stockform'}, {xtype: 'stockfinderapp-stockgrid'}
         ]
       });
-        }
+    }
     win.show();
   }
 });
 
-function createStockFinderForm(stockList) {
+
+Ext.define('Compass.ErpApp.Desktop.Applications.StockFinderApp.StockForm', {
+  extend: 'Ext.form.Panel',
+  xtype: 'stockfinderapp-stockform',
+  title: 'Stock Finder Form',
+  bodyPadding: 5,
+  width: 350,
+  height: 150,
+  url: '/erp_app/desktop/stock_finder_app/stock_details',
   
-  var form = Ext.create('Ext.form.Panel', {
-    title: 'Stock Finder Form',
-    bodyPadding: 5,
-    width: 350,
-    height: 150,
-    url: '/erp_app/desktop/stock_finder_app/stock_details',
-    
-    // Fields will be arranged vertically, stretched to full width
-    layout: 'anchor',
-    defaults: {
-      anchor: '100%'
-    },
-    
-    // The fields
-    defaultType: 'textfield',
-    items: [{
-      fieldLabel: 'Stock Symbol',
+  // Fields will be arranged vertically, stretched to full width
+  layout: 'anchor',
+  defaults: {
+    anchor: '100%'
+  },
+  
+  // The fields
+  defaultType: 'textfield',
+  items: [{
+    fieldLabel: 'Stock Symbol',
       name: 'stock_symbol',
-      allowBlank: false
-    }],
-    // Reset and Submit buttons
-    buttons: [{
-      text: 'Reset',
-      handler: function() {
-        this.up('form').getForm().reset();
-      }
-    }, {
-      text: 'See Details',
-      formBind: true, //only enabled once the form is valid
-      disabled: true,
-      handler: function() {
-        var form = this.up('form').getForm();
-        if (form.isValid()) {
-          form.submit({
-            success: function(form, action) {
-              stockList.store.loadData([],false);
-              Ext.each(action.result.data, function(stock, index){
-                stockList.store.add(stock);
-              });
-              Ext.Msg.alert('Success', JSON.stringify(action.result.data));
-            },
-            failure: function(form, action) {
-              Ext.Msg.alert('Failed', action.result.msg);
+    allowBlank: false
+  }],
+  // Reset and Submit buttons
+  buttons: [{
+    text: 'Reset',
+    handler: function() {
+      this.up('form').getForm().reset();
+    }
+  }, {
+    text: 'See Details',
+    formBind: true, //only enabled once the form is valid
+    disabled: true,
+    handler: function() {
+      var self = this;
+      var form = this.up('form').getForm();
+      if (form.isValid()) {
+        form.submit({
+          success: function(form, action) {
+            console.log("sucess");
+            var stockList = self.up('form').up('#stock_finder_app').down('stockfinderapp-stockgrid');
+            stockList.store.loadData([],false);
+            Ext.each(action.result.data, function(stock, index){
+              stockList.store.add(stock);
+            });
+            Ext.Msg.alert('Success', JSON.stringify(action.result.data));
+          },
+          failure: function(form, action) {
+            Ext.Msg.alert('Failed', action.result.msg);
             }
-          });
-        }
+        });
       }
-    }]
-  });
-  return form;
-}
+    }
+  }]
+});
 
 
-function createStockList(){
-  return Ext.create('Ext.grid.Panel' ,{
-    title: 'All Stock',
-    height: '400',
+Ext.define('Compass.ErpApp.Desktop.Applications.StockFinderApp.StockGrid',{
+  extend: 'Ext.grid.Panel',
+  title: 'All Stock',
+  height: '400',
+  xtype: 'stockfinderapp-stockgrid',
+  store: {
+    fields: ['company', 'day_high', 'change_points', 'change_percent']
+  },
+  
+  columns: [
+    {header: 'Company',  dataIndex: 'company',  flex: 1},
+    {header: 'Day High', dataIndex: 'day_high', flex: 1},
+    {header: 'Change Points', dataIndex: 'change_points', flex: 1},
+    {header: 'Change Perecent', dataIndex: 'change_percent', flex: 1}
+  ]
     
-    store: {
-      fields: ['company', 'day_high', 'change_points', 'change_percent']
-    },
-    
-    columns: [
-      {header: 'Company',  dataIndex: 'company',  flex: 1},
-      {header: 'Day High', dataIndex: 'day_high', flex: 1},
-      {header: 'Change Points', dataIndex: 'change_points', flex: 1},
-      {header: 'Change Perecent', dataIndex: 'change_percent', flex: 1}
-    ]
-    
-  });
-}
+});
+
 
   
   
