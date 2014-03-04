eddee59bfe1a2188a92779cc1f8007e69e4d1885
Anup Biswal <anup.craver@gmail.com>
2014-03-04 19:22:00 +0530

    CMS widget enhancements - Made form inline and decorated change values

--- app/widgets/stock_finder/helpers/view/stock_finder_view_helper.rb
+++ app/widgets/stock_finder/helpers/view/stock_finder_view_helper.rb
@@ -1,3 +1,13 @@
 module StockFinderViewHelper
   #add helper methods for your engines views
+
+  def decorate(value)
+    if value.to_i > 0
+      content_tag(:td, value, :style => 'color: green')
+    else
+      content_tag(:td, value, :style => 'color: red')
+    end
+  end
+
+
 end
